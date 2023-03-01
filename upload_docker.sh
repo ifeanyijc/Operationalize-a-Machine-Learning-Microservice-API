01e1b39560bdd61c29881aafe0012ad7f5a745ed
EC2 Default User <ec2-user@ip-172-31-17-5.ec2.internal>
2023-03-01 12:52:57 +0000

    update

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=ubahjc/sklearn
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u ubahjc
+docker tag ubahjc/sklearn $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push ubahjc/sklearn:latest
